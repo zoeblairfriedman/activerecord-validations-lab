2ec30c4c006d6fce2fb133fb2ff3be56caeda436
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-03-14 15:19:13 -0400

    commit tree 38d09ef70dd74310126e19f6dbe0bc7a781d3dfd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} must be Ficton or Non-Fiction"}
+    validate :is_clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+      ]
+    
+      def is_clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
 end
